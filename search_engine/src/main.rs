ce0ac86f5f952cd36019d925c7b26b481145f933
David Van <David@DavidVan.net>
2017-09-13 18:14:32 -0700

    Used 'use' keyword.

--- search_engine/src/main.rs
+++ search_engine/src/main.rs
@@ -1,5 +1,7 @@
 extern crate search_engine;
 
+use search_engine::porter_stemmer::{stemmer};
+
 fn main() {
-    println!("{}", search_engine::porter_stemmer::stemmer::hello());
+    println!("{}", stemmer::hello());
 }
