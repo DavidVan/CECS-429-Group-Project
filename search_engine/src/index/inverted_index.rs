3dc796936badadd1bb4ccb792ba0cf766e7c8591
Harold Agnote <haroldagnote38@hotmail.com>
2017-09-19 11:19:17 -0700

    Updated Inverted index

--- search_engine/src/index/inverted_index.rs
+++ search_engine/src/index/inverted_index.rs
@@ -1,37 +1,45 @@
 use std::collections::HashMap;
 
 pub struct InvertedIndex {
-    pub mIndex : HashMap<String, i32>,
+    pub mIndex : HashMap<String, Vec<i32>>,
 }
+
 
 impl InvertedIndex {
     pub fn new(&mut self) {
-        self.mIndex = HashMap::new(); 
+        self.mIndex = HashMap::new();
     }
-    pub fn addTerm(&self, term : &str, docID : i32)
+    pub fn addTerm(&mut self, term : &str, docID : i32)
     {
-        println!("Adding term {}{}", term, docID); 
+        println!("Adding term {}{}", term, docID);
 
-        if (self.mIndex.contains_key(term))
+        if self.mIndex.contains_key(term)
         {
-            if (!self.mIndex.get(term).contains(docID)) {
-                self.mIndex.get(term).push(docID); 
-            } 
+            let mut p = self.mIndex.get_mut(term);
+            let mut posting = p.as_mut().unwrap();
+            if !posting[posting.len() - 1] == docID {
+                posting.push(docID);
+            }
+            println!("Term {} added to {}", term, docID);
         }
         else
         {
-             
+            let mut posting = Vec::new();
+            posting.push(docID);
+            self.mIndex.insert(term.to_string(), posting); 
+            println!("Term {} added to {}", term, docID);
         }
     }
 
-    pub fn getPostings(&self, term : &str) -> Vec<i32> {
+    pub fn getPostings(&self, term : &str) -> &Vec<i32> {
         println!("Getting postings for term {}", term);
 
-        let x = vec![1,2,3];
-        return x;
+        let posting = self.mIndex.get(term).unwrap();
+        return posting;
+
     }
 
     pub fn getTermCount(&self) -> usize {
-        self.mIndex.len() 
+        self.mIndex.len()
     }
 }
