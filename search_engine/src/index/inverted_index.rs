d0b4f47be30818bde5284c05f142f93225b82fac
Harold Agnote <haroldagnote38@hotmail.com>
2017-09-19 12:14:09 -0700

    Seemingly finished NaiveInvertedIndex

--- search_engine/src/index/inverted_index.rs
+++ search_engine/src/index/inverted_index.rs
@@ -4,20 +4,19 @@ pub struct InvertedIndex {
     pub mIndex : HashMap<String, Vec<i32>>,
 }
 
-
 impl InvertedIndex {
     pub fn new(&mut self) {
         self.mIndex = HashMap::new();
     }
     pub fn addTerm(&mut self, term : &str, docID : i32)
     {
-        println!("Adding term {}{}", term, docID);
+        println!("Adding term {} to {}", term, docID);
 
         if self.mIndex.contains_key(term)
         {
             let mut p = self.mIndex.get_mut(term);
             let mut posting = p.as_mut().unwrap();
-            if !posting[posting.len() - 1] == docID {
+            if posting[posting.len() - 1] != docID {
                 posting.push(docID);
             }
             println!("Term {} added to {}", term, docID);
@@ -34,12 +33,21 @@ impl InvertedIndex {
     pub fn getPostings(&self, term : &str) -> &Vec<i32> {
         println!("Getting postings for term {}", term);
 
-        let posting = self.mIndex.get(term).unwrap();
-        return posting;
+        self.mIndex.get(term).unwrap()
 
     }
 
     pub fn getTermCount(&self) -> usize {
         self.mIndex.len()
+    }
+
+    pub fn getDictionary(&self) -> Vec<&String> {
+        let mut dictionary = Vec::new();
+
+        for term in self.mIndex.keys() {
+            dictionary.push(term) 
+        }
+
+        return dictionary;
     }
 }
