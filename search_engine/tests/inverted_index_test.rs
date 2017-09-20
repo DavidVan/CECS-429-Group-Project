365f1bbf7174facff8fa8015540fc8a3895f3371
Harold Agnote <haroldagnote38@hotmail.com>
2017-09-20 11:38:35 -0700

    Cleaned up main

--- search_engine/tests/inverted_index_test.rs
+++ search_engine/tests/inverted_index_test.rs
@@ -13,5 +13,14 @@ fn inverted_index() {
     for id in 1..10 {
         inverted_index.addTerm(&term, id);
         println!("There are {} in index", inverted_index.getTermCount());
+
+        let postings = inverted_index.getPostings(&term);
+
+        print!("{} : ", term);
+
+        for p in postings {
+            print!("{} ", p);
+        }
+        println!("\n");
     }
 }
