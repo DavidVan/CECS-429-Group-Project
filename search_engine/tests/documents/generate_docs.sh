c2c9daa2f747d03ffcd047543631fa670d3b7818
Harold Agnote <haroldagnote38@hotmail.com>
2017-09-26 11:29:33 -0700

    Fixed some stuff with Positional Index and working on unit test

---
+++ search_engine/tests/documents/generate_docs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm doc*
+
+for i in {1..5}
+do
+    touch doc$i.txt
+done
+
+wordBank=('alpha' 'bravo' 'charlie' 'delta' 'echo' 'foxtrot' 'golf' 'hotel' 'india' 'juliet' 'kilo' 'lima' 'mike' 'november' 'oscar' 'papa' 'quebec' 'romeo'
+'sierra' 'tango' 'uniform' 'victor' 'whiskey' 'xray' 'yankee' 'zulu')
+
+for doc in *
+do
+    numOfWords=$(((RANDOM % 5) + 10))
+
+    for j in $(seq 0 $numOfWords)
+    do
+        index=$((RANDOM % 26))
+        word=${wordBank[$index]}
+        echo "$word" >> "$doc"
+    done
+    
+done
