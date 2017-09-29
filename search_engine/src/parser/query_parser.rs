14e00391445f2c811cb564193e4b9693b79de0bd
David Van <David@DavidVan.net>
2017-09-29 01:55:37 -0700

    Almost working... So close yet so far...

--- search_engine/src/parser/query_parser.rs
+++ search_engine/src/parser/query_parser.rs
@@ -1,5 +1,6 @@
 use std::io::{self, Read};
 use std::process::exit;
+use std::option;
 
 pub struct QueryParser {
 
@@ -10,39 +11,61 @@ impl QueryParser {
         QueryParser {}
     }
 
-    pub fn group_tokens(&self, input: &Vec<String>) -> Vec<Vec<String>> {
+    pub fn group_tokens(&self, input: &Vec<String>, previous_token_head: Option<&String>) -> Vec<Vec<String>> {
         let mut query_group = Vec::new();
         let mut token_group = Vec::new();
-        let mut previous_token = String::new();
+        let mut previous_token = String::new(); /*match previous_token_head {
+            Some(token) => token.clone(),
+            None => String::new(),
+        };*/
+        println!("GROUP USING PREV TOK {}", previous_token);
         for token in input {
             //println!("This is a token: {}", token);
             if token.len() == 1 && token.starts_with('+') {
                 query_group.push(token_group);
                 token_group = Vec::new();
+                previous_token = String::new(); /*match previous_token_head {
+                    Some(token) => token.clone(),
+                    None => String::new(),
+                };*/
                 continue;
             }
             else if token.starts_with('(') && token.ends_with(')') && previous_token.len() != 0 {
-                if (token.chars().nth(0).unwrap() == '\"') {
-                    println!("quote mark");
-                }
-                else {
-                    let mut inner_query : String = token.as_str().chars().skip(1).collect(); 
-                    inner_query = inner_query.chars().take(inner_query.len() - 1).collect();
-                    println!("{}", inner_query);
-                    // token is already (term1 + term2) use regex to remove ()+?
-                    let inner_token = self.tokenize_query(inner_query.as_str());
-                    let inner_group = self.group_tokens(&inner_token);
-                    for group in inner_group {
-                        println!("Inner Group: {:?}", group);
+                let mut inner_query : String = token.as_str().chars().skip(1).collect(); 
+                println!("Multiply using {}", previous_token);
+                inner_query = inner_query.chars().take(inner_query.len() - 1).collect();
+                println!("Inner Query: {}", inner_query);
+                // token is already (term1 + term2) use regex to remove ()+?
+                let inner_query = self.tokenize_query(inner_query.as_str());
+                let inner_group = self.group_tokens(&inner_query, Some(&previous_token));
+                for inner_token_group in inner_group {
+                    for mut inner_token in inner_token_group {
+                        println!("Inner Token: {:?}", inner_token);
+                        if inner_token.len() == 1 && inner_token.starts_with("+") {
+                            //continue;
+                            break;
+                        }
+                        inner_token = format!("{} {}", previous_token, inner_token);
+                        token_group.push(inner_token);
                     }
-                    // maybe I should push into the vec?
                 }
             }
             token_group.push(token.clone());
-            previous_token = token.clone();
+            //previous_token.push(' ');
+            //previous_token.push_str(token);
+            previous_token = String::new();
         }
         query_group.push(token_group);
-        query_group
+        println!("Query Group: {:?}", query_group);
+        let mut new_query_group = Vec::new();
+        for groups in query_group.clone() {
+            let mut join_vec = Vec::new();
+            let join_tokens = groups.join(" ");
+            println!("JOIN TOKENS {}", join_tokens);
+            join_vec.push(join_tokens);
+            new_query_group.push(join_vec);
+        }
+        new_query_group
     }
 
     pub fn tokenize_query(&self, input: &str) -> Vec<String> {
@@ -51,11 +74,11 @@ impl QueryParser {
         while let Some(token) = input_iter.next() {
             if token.starts_with("\"") && !token.starts_with("(\"") {
                 let mut phrase_literal = String::new();
-                phrase_literal.push_str(token.trim_matches('\"'));
+                phrase_literal.push_str(token);
                 phrase_literal.push_str(" ");
                 while let Some(next_token) = input_iter.next() {
                     if next_token.ends_with("\"") && !next_token.ends_with("\")") {
-                        phrase_literal.push_str(next_token.trim_matches('\"'));
+                        phrase_literal.push_str(next_token);
                         break;
                     }
                     phrase_literal.push_str(next_token);
--- search_engine/tests/query_parser.rs
+++ search_engine/tests/query_parser.rs
@@ -5,11 +5,17 @@ use search_engine::parser::query_parser::QueryParser;
 #[test]
 fn test_parser() {
     let parser = QueryParser::new();
-    let query = "testing 1 2 + 3 \"hello 世界 world\" hi + \"hello 世界 world\" test (hello + \"hello world\" (inner + \"hello world\" \"(still + in + same + group)\"))";
+    let query = "testing 1 2 + 3 \"hello1 世界 world\" hi + \"hello2 世界 world\" test (hello3 + \"hello4 world\" (inner + \"hello5 world\" \"(still + in + same + group)\"))";
     println!("Original Query: {}", query);
     let tokens = parser.tokenize_query(query);
-    let groups = parser.group_tokens(&tokens);
+    let groups = parser.group_tokens(&tokens, None);
+    println!("ALMOST FINISHED");
     for token_group in groups {
-        println!("Group {:?}", token_group);
+        for tokens in token_group {
+            println!("OUR FINAL FINISHED TOKENS {} ", tokens);
+        }
+        //println!("Group {:?}", token_group);
     }
+    //println!("FINAL FUCKING RESULT {:?}" groups);
+    println!("FINISHED");
 }
