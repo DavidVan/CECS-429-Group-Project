ac3230518297360c078a441bad7306e0a3955aee
David Van <David@DavidVan.net>
2017-09-29 19:58:24 -0700

    Almost there... Lots of commented out code that is useless...

--- search_engine/src/parser/query_parser.rs
+++ search_engine/src/parser/query_parser.rs
@@ -11,6 +11,65 @@ impl QueryParser {
         QueryParser {}
     }
 
+    pub fn multiply_token(&self, multiplier: String, multiplicand: &Vec<&str>) -> Vec<String> {
+        let mut results = Vec::new();
+        for item in multiplicand {
+            results.push(multiplier.clone() + " " + item);
+        }
+        results // All Results need to be OR'd / Insert '+' between.
+    }
+
+    pub fn process_query(&self, input: &str) -> Vec<Vec<String>> {
+        let mut query = input;
+        let mut final_query = Vec::new();
+
+        let mut query_iter = query.split_whitespace(); // Split on whitespace
+        let mut preprocessed_query = Vec::new(); // Hold tokens until we reach a '+' sign...
+        while let Some(sub_query) = query_iter.next() { // Go through every token / sub-query
+            if sub_query.len() == 1 && sub_query.starts_with("+") { // If it's a '+' sign...
+                final_query.push(preprocessed_query); // Push into our results vector...
+                preprocessed_query = Vec::new(); // Reset it
+                continue; // Skip '+' signs...
+            }
+            if sub_query.starts_with("(") {
+                //let mut extended_query : String = sub_query.clone().chars().skip(1).take(sub_query.len() - 1).collect();
+                // Right now, I only have "(hello3"...
+                // Get the rest of it, and make sure I get correct number of parenthesis...
+                let mut query_builder = Vec::new();
+                query_builder.push(sub_query);
+                let mut left_parenthesis_counter = 1;
+                let mut right_parenthesis_counter = 0;
+                let mut next_sub_query = query_iter.next().unwrap();
+                while left_parenthesis_counter > right_parenthesis_counter {
+                    query_builder.push(next_sub_query.clone());
+                    if next_sub_query.starts_with("(") {
+                        left_parenthesis_counter += 1;
+                    }
+                    else if next_sub_query.ends_with(")") {
+                        right_parenthesis_counter += 1;
+                        left_parenthesis_counter -= 1;
+                    }
+                    next_sub_query = query_iter.next().unwrap();
+                }
+                preprocessed_query.push(query_builder.join(" "));
+                println!("Built Query: {}", query_builder.join(" "));
+                final_query.push(preprocessed_query);
+                preprocessed_query = Vec::new();
+                continue;
+                //println!("Extended Query: {}", extended_query);
+                //let multiplier = preprocessed_query.join(" ");
+                //println!("Multiplier: {}", multiplier);
+                //let multiply = self.multiply_token(multiplier, extended_query); 
+            }
+            preprocessed_query.push(String::from(sub_query));
+        }
+        final_query.push(preprocessed_query);
+
+        for i in final_query.clone() {
+            println!("Final Query: {:?}", i);
+        }
+        final_query
+    }
     /*pub fn group_tokens(&self, input: &Vec<String>, previous_token_head: Option<&String>) -> Vec<Vec<String>> {
         let mut query_group = Vec::new();
         let mut token_group = Vec::new();
@@ -119,10 +178,17 @@ impl QueryParser {
         query_group
     }*/
 
-    pub fn tokenize_query(&self, input: &str) -> Vec<String> {
+    /*pub fn tokenize_query(&self, input: &str, recursive_steps: u32) -> Vec<String> {
         let mut tokens = Vec::new();
+        println!("ABC INPUT {}", input);
         let mut input_iter = input.split_whitespace();
+        let mut multiply_token = Vec::new();
         while let Some(token) = input_iter.next() {
+            if token.len() == 1 && token.starts_with("+") {
+                tokens.push(String::from(token));
+                multiply_token = Vec::new();
+                continue;
+            }
             if token.starts_with("\"") {
                 let mut phrase_literal = String::new();
                 phrase_literal.push_str(token);
@@ -135,7 +201,8 @@ impl QueryParser {
                     phrase_literal.push_str(next_token);
                     phrase_literal.push_str(" ");
                 }
-                tokens.push(phrase_literal);
+                tokens.push(phrase_literal.clone());
+                multiply_token.push(phrase_literal.clone());
             }
             else if token.starts_with("(") {
                 let mut inner_query_literal = String::new();
@@ -149,9 +216,44 @@ impl QueryParser {
                     inner_query_literal.push_str(next_token);
                     inner_query_literal.push_str(" ");
                 }
-                tokens.push(inner_query_literal);
-            } else {
+                //tokens.push(inner_query_literal.clone());
+                let inner_query_literal = inner_query_literal.trim_left_matches("(").trim_right_matches(")");
+                for i in 0..recursive_steps {
+                    //tokens.pop();
+                }
+                let inner_tokens = self.tokenize_query(inner_query_literal, recursive_steps + 1);
+                let mut inner_word_group = Vec::new();
+                let mut inner_group = Vec::new();
+                for inner_token in inner_tokens {
+                    println!("These are the inner tokens: {}", inner_token);
+                    if inner_token.len() == 1 && inner_token.starts_with("+") {
+                        let combined_inner_word_group_tokens = inner_word_group.join(" ");
+                        println!("These are the combined tokens: {}", combined_inner_word_group_tokens);
+                        inner_group.push(combined_inner_word_group_tokens);
+                        inner_word_group = Vec::new();
+                        continue;
+                    }
+                    inner_word_group.push(inner_token.clone());
+                }
+                let combined_inner_word_group_tokens = inner_word_group.join(" ");
+                println!("These are the combined tokens: {}", combined_inner_word_group_tokens);
+                inner_group.push(combined_inner_word_group_tokens);
+
+                println!("INNER GROUP {:?}", inner_group);
+                for i in 0..multiply_token.len() {
+                    tokens.pop();
+                }
+                for inner_group_token in inner_group {
+                    println!("Multiplying using {}", multiply_token.join(" "));
+                    println!("MULTIPLYING {}", inner_group_token);
+                    tokens.push(String::from("+"));
+                    let multiplied_token = multiply_token.join(" ") + " " + &inner_group_token;
+                    tokens.push(multiplied_token);
+                }
+            }
+            else {
                 tokens.push(String::from(token));
+                multiply_token.push(String::from(token));
             }
         }
         /*match input.chars().nth(0) {
@@ -164,5 +266,5 @@ impl QueryParser {
           }*/
         println!("FINAL RESULT: {:?}", tokens);
         tokens
-    }
+    }*/
 }
--- search_engine/tests/query_parser.rs
+++ search_engine/tests/query_parser.rs
@@ -5,7 +5,14 @@ use search_engine::parser::query_parser::QueryParser;
 #[test]
 fn test_parser() {
     let parser = QueryParser::new();
-    let query = "testing 1 2 + 3 \"hello1 世界 world\" hi + \"hello2 世界 world\" test (hello3 + \"hello4 world\" (inner + \"hello5 world\" \"(still + in + same + group)\"))";
+    let query = "testing 1 2 + 3 \"hello1 世界 world\" hi + \"hello2 世界 world\" test (hello3 + \"hello4 world\" (inner + \"hello5 world\" \"(still + in + same + group)\")) + hello (banana + strawberry) + bye";
+    println!("Original Query: \n{}", query);
+    let multiply_test = vec!["this", "that", "\"(who + am + i)\""];
+    let multiply = parser.multiply_token(String::from("multiply"), &multiply_test);
+    for item in multiply {
+        println!("Multiply Item: {}", item);
+    }
+    let final_query = parser.process_query(query);
     /*println!("Original Query: {}", query);
     let tokens = parser.tokenize_query(query);
     println!("ALL ZE TOKENS: {:?}", tokens);
@@ -18,10 +25,10 @@ fn test_parser() {
         //println!("Group {:?}", token_group);
     }
     println!("FINISHED");*/
-    let tokens = parser.tokenize_query(query);
-    for token in tokens {
-        println!("TOKEN: {:?}", token);
-    }
+    //let tokens = parser.tokenize_query(query, 1);
+    //for token in tokens {
+     //   println!("TOKEN: {:?}", token);
+   // }
     //let query_group = parser.group_tokens(&tokens);
     /*for token_group in query_group {
         println!("Token Group: {}", token_group);
