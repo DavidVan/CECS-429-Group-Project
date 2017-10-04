85a9807b51377a62c34dbea1aa1cc8a730652400
Harold Agnote <haroldagnote38@hotmail.com>
2017-10-03 18:02:32 -0700

    Added unit test for near operator

--- search_engine/tests/query_processor.rs
+++ search_engine/tests/query_processor.rs
@@ -20,29 +20,50 @@ fn test_queries() {
     let test_query_2 = "alpha bravo"; // Tests query with AND operator
     let test_query_3 = "alpha -november"; // Tests query with NOT operator
     let test_query_4 = "alpha + mike"; // Tests query with OR operator
+    let test_query_5 = "kilo NEAR/3 mike"; // Tests query with NEAR operator
 
     let result_query_1 = query_processor::process_query(&test_query_1, &index, &docid_file);
     let result_key_1 = vec!["doc1.txt", "doc2.txt", "doc5.txt"];
+    let result_key_1_size = result_key_1.len();
 
     let result_query_2 = query_processor::process_query(&test_query_2, &index, &docid_file);
     let result_key_2 = vec!["doc1.txt", "doc2.txt"];
+    let result_key_2_size = result_key_2.len();
 
     let result_query_3 = query_processor::process_query(&test_query_3, &index, &docid_file);
 
     let result_query_4 = query_processor::process_query(&test_query_4, &index, &docid_file);
     let result_key_4 = vec!["doc1.txt", "doc2.txt", "doc3.txt", "doc4.txt", "doc5.txt"];
+    let result_key_4_size = result_key_4.len();
+
+    let result_query_5 = query_processor::process_query(&test_query_5, &index, &docid_file);
+    let result_key_5 = vec!["doc2.txt"];
+    let result_key_5_size = result_key_5.len();
+    
 
     for result in result_key_1 {
         assert!(result_query_1.contains(result));
     }
 
+    assert_eq!(result_key_1_size, result_query_1.len());
+
     for result in result_key_2 {
         assert!(result_query_2.contains(result));
     }
+
+    assert_eq!(result_key_2_size, result_query_2.len());
 
     assert!(result_query_3.is_empty());
 
     for result in result_key_4 {
         assert!(result_query_4.contains(result));
     }
+
+    assert_eq!(result_key_4_size, result_query_4.len());
+
+    for result in result_key_5 {
+        assert!(result_query_5.contains(result));
+    }
+
+    assert_eq!(result_key_5_size, result_query_5.len());
 }
