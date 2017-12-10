3b7d72ca881e9a491d751d36949bc68a41487490
David Van <David@DavidVan.net>
2017-12-10 05:04:23 -0800

    Improved performance by almost 3x!

--- search_engine/src/classifier/bayesian_classifier.rs
+++ search_engine/src/classifier/bayesian_classifier.rs
@@ -1,3 +1,4 @@
+use std::time::{Duration, Instant};
 use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::collections::BinaryHeap;
@@ -61,24 +62,39 @@ impl<'a> BayesianClassifier<'a> {
 
     pub fn build_discriminating_vocab_set(&self, k: u32) -> Vec<TermClassScore> {
         let all_vocabulary = self.get_all_vocab();
-        let classes = vec!(DocumentClass::Hamilton, DocumentClass::Jay, DocumentClass::Madison);
 
         let mut priority_queue: BinaryHeap<TermClassScore> = BinaryHeap::new();
-
-        for class in classes {
-            for term in &all_vocabulary {
-                let score = match self.calculate_mutual_information_score(term.clone(), class) {
-                    Ok(score) => match TermClassScore::new(score, term.clone(), class) {
-                        Some(thing) => {
-                            println!("Adding to priority queue: {:?}", thing);
-                            priority_queue.push(thing);
+        let time = Instant::now();
+        for term in &all_vocabulary {
+            match self.calculate_mutual_information_score(term.clone()) {
+                Ok(score) => {
+                    let (score_hamilton, score_jay, score_madison) = score;
+                    match TermClassScore::new(score_hamilton, term.clone(), DocumentClass::Hamilton) {
+                        Some(score) => {
+                            println!("Adding to priority queue: {:?}", score);
+                            priority_queue.push(score);
                         },
                         None => continue
-                    },
-                    Err(error) => panic!("There was an error calculating the score for term {}, class {:?}. The error is: {}", term, class, error),
-                };
-            }
+                    };
+                    match TermClassScore::new(score_jay, term.clone(), DocumentClass::Jay) {
+                        Some(score) => {
+                            println!("Adding to priority queue: {:?}", score);
+                            priority_queue.push(score);
+                        },
+                        None => continue
+                    };
+                    match TermClassScore::new(score_madison, term.clone(), DocumentClass::Madison) {
+                        Some(score) => {
+                            println!("Adding to priority queue: {:?}", score);
+                            priority_queue.push(score);
+                        },
+                        None => continue
+                    };
+                },
+                Err(error) => panic!("There was an error calculating the score for term {}. The error is: {}", term, error),
+            };
         }
+        println!("Time taken to build discriminating vocab: {}", time.elapsed().as_secs());
         let mut discriminating_vocab = Vec::new();
         for i in 0..k {
             match priority_queue.pop() {
@@ -108,7 +124,7 @@ impl<'a> BayesianClassifier<'a> {
         }
     }
 
-    fn get_n_00(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // N00, total number of documents that DO NOT contain term t and NOT in class c.
+    fn get_n_00(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // N00, total number of documents that DO NOT contain term t and NOT in class c.
         let hamilton_total_num = self.index_hamilton.get_num_documents().expect("No Documents found!"); // Nc, total number of documents for class. 
         let jay_total_num = self.index_jay.get_num_documents().expect("No Documents found!"); // Nc, total number of documents for class. 
         let madison_total_num = self.index_madison.get_num_documents().expect("No Documents found!"); // Nc, total number of documents for class. 
@@ -135,16 +151,16 @@ impl<'a> BayesianClassifier<'a> {
         let jay_num_without_term = jay_total_num - num_in_jay_for_term;
         let madison_num_without_term = madison_total_num - num_in_madison_for_term;
 
-        let n_00 = match *class {
-            DocumentClass::Hamilton => jay_num_without_term + madison_num_without_term,
-            DocumentClass::Jay => hamilton_num_without_term + madison_num_without_term,
-            DocumentClass::Madison => hamilton_num_without_term + jay_num_without_term,
-        };
+        let n_00_hamilton = jay_num_without_term + madison_num_without_term;
+        let n_00_jay = hamilton_num_without_term + madison_num_without_term;
+        let n_00_madison = hamilton_num_without_term + jay_num_without_term;
+
+        let n_00 = (n_00_hamilton, n_00_jay, n_00_madison);
 
         Ok(n_00)
     }
 
-    fn get_n_01(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // N01, total number of documents that DO contain term t and NOT in class c.
+    fn get_n_01(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // N01, total number of documents that DO contain term t and NOT in class c.
         let hamilton_postings_for_term = self.index_hamilton.get_postings_no_positions(term);
         let num_in_hamilton_for_term = match hamilton_postings_for_term {
             Ok(postings) => postings.len() as u32,
@@ -163,16 +179,16 @@ impl<'a> BayesianClassifier<'a> {
             Err(_) => 0,
         };
 
-        let n_01 = match *class {
-            DocumentClass::Hamilton => num_in_jay_for_term + num_in_madison_for_term,
-            DocumentClass::Jay => num_in_hamilton_for_term + num_in_madison_for_term,
-            DocumentClass::Madison => num_in_hamilton_for_term + num_in_jay_for_term,
-        };
+        let n_01_hamilton = num_in_jay_for_term + num_in_madison_for_term;
+        let n_01_jay = num_in_hamilton_for_term + num_in_madison_for_term;
+        let n_01_madison = num_in_hamilton_for_term + num_in_jay_for_term;
+
+        let n_01 = (n_01_hamilton, n_01_jay, n_01_madison);
 
         Ok(n_01)
     }
 
-    fn get_n_10(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // N10, total number of documents that DO NOT contain term t but IS in class c.
+    fn get_n_10(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // N10, total number of documents that DO NOT contain term t but IS in class c.
         let hamilton_total_num = self.index_hamilton.get_num_documents().expect("No Documents found!"); // Nc, total number of documents for class. 
         let jay_total_num = self.index_jay.get_num_documents().expect("No Documents found!"); // Nc, total number of documents for class. 
         let madison_total_num = self.index_madison.get_num_documents().expect("No Documents found!"); // Nc, total number of documents for class. 
@@ -199,16 +215,16 @@ impl<'a> BayesianClassifier<'a> {
         let jay_num_without_term = jay_total_num - num_in_jay_for_term;
         let madison_num_without_term = madison_total_num - num_in_madison_for_term;
 
-        let n_10 = match *class {
-            DocumentClass::Hamilton => hamilton_num_without_term,
-            DocumentClass::Jay => jay_num_without_term,
-            DocumentClass::Madison => madison_num_without_term,
-        };
+        let n_10_hamilton = hamilton_num_without_term;
+        let n_10_jay = jay_num_without_term;
+        let n_10_madison = madison_num_without_term;
+
+        let n_10 = (n_10_hamilton, n_10_jay, n_10_madison);
 
         Ok(n_10)
     }
 
-    fn get_n_11(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // N11, total number of documents that DO contain term t and IS in class c.
+    fn get_n_11(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // N11, total number of documents that DO contain term t and IS in class c.
         let hamilton_postings_for_term = self.index_hamilton.get_postings_no_positions(term);
         let num_in_hamilton_for_term = match hamilton_postings_for_term {
             Ok(postings) => postings.len() as u32,
@@ -227,118 +243,147 @@ impl<'a> BayesianClassifier<'a> {
             Err(_) => 0,
         };
 
-        let n_11 = match *class {
-            DocumentClass::Hamilton => num_in_hamilton_for_term,
-            DocumentClass::Jay => num_in_jay_for_term,
-            DocumentClass::Madison => num_in_madison_for_term,
-        };
+        let n_11_hamilton = num_in_hamilton_for_term;
+        let n_11_jay = num_in_jay_for_term;
+        let n_11_madison = num_in_madison_for_term;
+
+        let n_11 = (n_11_hamilton, n_11_jay, n_11_madison);
 
         Ok(n_11)
     }
 
-    fn get_n_0X(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // N0X, N00 + N01
-        let n_00 = match self.get_n_00(term, class) {
+    fn get_n_0X(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // N0X, N00 + N01
+        let (n_00_hamilton, n_00_jay, n_00_madison) = match self.get_n_00(term) {
             Ok(n_00) => n_00,
             Err(_) => panic!("Something happened when calculating N00!"),
         };
-        let n_01 = match self.get_n_01(term, class) {
+        let (n_01_hamilton, n_01_jay, n_01_madison) = match self.get_n_01(term) {
             Ok(n_01) => n_01,
             Err(_) => panic!("Something happened when calculating N01!"),
         };
 
-        let n_0X = n_00 + n_01;
+        let n_0X_hamilton = n_00_hamilton + n_01_hamilton;
+        let n_0X_jay = n_00_jay + n_01_jay;
+        let n_0X_madison = n_00_madison + n_01_madison;
+
+        let n_0X = (n_0X_hamilton, n_0X_jay, n_0X_madison);
 
         Ok(n_0X)
     }
 
-    fn get_n_X0(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // NX0, N00 + N10
-        let n_00 = match self.get_n_00(term, class) {
+    fn get_n_X0(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // NX0, N00 + N10
+        let (n_00_hamilton, n_00_jay, n_00_madison) = match self.get_n_00(term) {
             Ok(n_00) => n_00,
             Err(_) => panic!("Something happened when calculating N00!"),
         };
-        let n_11 = match self.get_n_11(term, class) {
-            Ok(n_11) => n_11,
-            Err(_) => panic!("Something happened when calculating N11!"),
+        let (n_10_hamilton, n_10_jay, n_10_madison) = match self.get_n_10(term) {
+            Ok(n_10) => n_10,
+            Err(_) => panic!("Something happened when calculating N10!"),
         };
 
-        let n_X0 = n_00 + n_11;
+        let n_X0_hamilton = n_00_hamilton + n_10_hamilton;
+        let n_X0_jay = n_00_jay + n_10_jay;
+        let n_X0_madison = n_00_madison + n_10_madison;
+
+        let n_X0 = (n_X0_hamilton, n_X0_jay, n_X0_madison);
 
         Ok(n_X0)
     }
 
-    fn get_n_1X(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // N1X, N10 + N11
-        let n_10 = match self.get_n_10(term, class) {
+    fn get_n_1X(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // N1X, N10 + N11
+        let (n_10_hamilton, n_10_jay, n_10_madison) = match self.get_n_10(term) {
             Ok(n_10) => n_10,
             Err(_) => panic!("Something happened when calculating N10!"),
         };
-        let n_11 = match self.get_n_11(term, class) {
+        let (n_11_hamilton, n_11_jay, n_11_madison) = match self.get_n_11(term) {
             Ok(n_11) => n_11,
             Err(_) => panic!("Something happened when calculating N11!"),
         };
 
-        let n_1X = n_10 + n_11;
+        let n_1X_hamilton = n_10_hamilton + n_11_hamilton;
+        let n_1X_jay = n_10_jay + n_11_jay;
+        let n_1X_madison = n_10_madison + n_11_madison;
+
+        let n_1X = (n_1X_hamilton, n_1X_jay, n_1X_madison);
 
         Ok(n_1X)
     }
 
-    fn get_n_X1(&self, term: &str, class: &DocumentClass) -> Result<u32, &'static str> { // NX1, N01 + N11
-        let n_01 = match self.get_n_01(term, class) {
+    fn get_n_X1(&self, term: &str) -> Result<(u32, u32, u32), &'static str> { // NX1, N01 + N11
+        let (n_01_hamilton, n_01_jay, n_01_madison) = match self.get_n_01(term) {
             Ok(n_01) => n_01,
             Err(_) => panic!("Something happened when calculating N01!"),
         };
-        let n_11 = match self.get_n_11(term, class) {
+        let (n_11_hamilton, n_11_jay, n_11_madison) = match self.get_n_11(term) {
             Ok(n_11) => n_11,
             Err(_) => panic!("Something happened when calculating N11!"),
         };
 
-        let n_X1 = n_01 + n_11;
+        let n_X1_hamilton = n_01_hamilton + n_11_hamilton;
+        let n_X1_jay = n_01_jay + n_11_jay;
+        let n_X1_madison = n_01_madison + n_11_madison;
+
+        let n_X1 = (n_X1_hamilton, n_X1_jay, n_X1_madison);
 
         Ok(n_X1)
     }
 
-    fn calculate_mutual_information_score(&self, term: String, class: DocumentClass) -> Result<f64, &'static str> {
+    fn calculate_mutual_information_score(&self, term: String) -> Result<(f64, f64, f64), &'static str> {
         let n = self.get_all_vocab().len() as u32;
 
-        let n_00 = match self.get_n_00(&term, &class) {
+        let (n_00_hamilton, n_00_jay, n_00_madison) = match self.get_n_00(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_00! Error: {}", error),
         };
-        let n_01 = match self.get_n_01(&term, &class) {
+        let (n_01_hamilton, n_01_jay, n_01_madison) = match self.get_n_01(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_01! Error: {}", error),
         };
-        let n_10 = match self.get_n_10(&term, &class) {
+        let (n_10_hamilton, n_10_jay, n_10_madison) = match self.get_n_10(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_10! Error: {}", error),
         };
-        let n_11 = match self.get_n_11(&term, &class) {
+        let (n_11_hamilton, n_11_jay, n_11_madison) = match self.get_n_11(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_11! Error: {}", error),
         };
-        
-        let n_0X = match self.get_n_0X(&term, &class) {
+        let (n_0X_hamilton, n_0X_jay, n_0X_madison) = match self.get_n_0X(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_0X! Error: {}", error),
         };
-        let n_X0 = match self.get_n_X0(&term, &class) {
+        let (n_X0_hamilton, n_X0_jay, n_X0_madison) = match self.get_n_X0(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_X0! Error: {}", error),
         };
-        let n_1X = match self.get_n_1X(&term, &class) {
+        let (n_1X_hamilton, n_1X_jay, n_1X_madison) = match self.get_n_1X(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_1X! Error: {}", error),
         };
-        let n_X1 = match self.get_n_X1(&term, &class) {
+        let (n_X1_hamilton, n_X1_jay, n_X1_madison) = match self.get_n_X1(&term) {
             Ok(value) => value,
             Err(error) => panic!("Something happened when calculating N_X1! Error: {}", error),
         };
 
-        let first_term = (n_11 as f64 / n as f64) * ((n * n_11) as f64 / (n_1X * n_X1) as f64).log2();
-        let second_term = (n_10 as f64 / n as f64) * ((n * n_10) as f64 / (n_1X * n_X0) as f64).log2();
-        let third_term = (n_01 as f64 / n as f64) * ((n * n_01) as f64 / (n_0X * n_X1) as f64).log2();
-        let fourth_term = (n_00 as f64 / n as f64) * ((n * n_00) as f64 / (n_0X * n_X0) as f64).log2();
+        let first_term_hamilton = (n_11_hamilton as f64 / n as f64) * ((n * n_11_hamilton) as f64 / (n_1X_hamilton * n_X1_hamilton) as f64).log2();
+        let second_term_hamilton = (n_10_hamilton as f64 / n as f64) * ((n * n_10_hamilton) as f64 / (n_1X_hamilton * n_X0_hamilton) as f64).log2();
+        let third_term_hamilton = (n_01_hamilton as f64 / n as f64) * ((n * n_01_hamilton) as f64 / (n_0X_hamilton * n_X1_hamilton) as f64).log2();
+        let fourth_term_hamilton = (n_00_hamilton as f64 / n as f64) * ((n * n_00_hamilton) as f64 / (n_0X_hamilton * n_X0_hamilton) as f64).log2();
 
-        let score = first_term + second_term + third_term + fourth_term;
+        let first_term_jay = (n_11_jay as f64 / n as f64) * ((n * n_11_jay) as f64 / (n_1X_jay * n_X1_jay) as f64).log2();
+        let second_term_jay = (n_10_jay as f64 / n as f64) * ((n * n_10_jay) as f64 / (n_1X_jay * n_X0_jay) as f64).log2();
+        let third_term_jay = (n_01_jay as f64 / n as f64) * ((n * n_01_jay) as f64 / (n_0X_jay * n_X1_jay) as f64).log2();
+        let fourth_term_jay = (n_00_jay as f64 / n as f64) * ((n * n_00_jay) as f64 / (n_0X_jay * n_X0_jay) as f64).log2();
+
+        let first_term_madison = (n_11_madison as f64 / n as f64) * ((n * n_11_madison) as f64 / (n_1X_madison * n_X1_madison) as f64).log2();
+        let second_term_madison = (n_10_madison as f64 / n as f64) * ((n * n_10_madison) as f64 / (n_1X_madison * n_X0_madison) as f64).log2();
+        let third_term_madison = (n_01_madison as f64 / n as f64) * ((n * n_01_madison) as f64 / (n_0X_madison * n_X1_madison) as f64).log2();
+        let fourth_term_madison = (n_00_madison as f64 / n as f64) * ((n * n_00_madison) as f64 / (n_0X_madison * n_X0_madison) as f64).log2();
+
+        let score_hamilton = first_term_hamilton + second_term_hamilton + third_term_hamilton + fourth_term_hamilton;
+        let score_jay = first_term_jay + second_term_jay + third_term_jay + fourth_term_jay;
+        let score_madison = first_term_madison + second_term_madison + third_term_madison + fourth_term_madison;
+
+        let score = (score_hamilton, score_jay, score_madison);
 
         Ok(score)
     }
