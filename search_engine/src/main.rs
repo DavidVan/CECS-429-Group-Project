b22dbe98f904edff21270c0b57b36ad34e0eac33
David Van <David@DavidVan.net>
2017-12-10 21:56:36 -0800

    Fixed typo.

--- search_engine/src/main.rs
+++ search_engine/src/main.rs
@@ -484,7 +484,7 @@ fn print_help(){
     println!(":mode b || :mode boolean - Use Boolean Retrieval Method");
     println!(":mode r || :mode ranked - Use Ranked Retrieval Method");
     println!(":scheme d || :scheme default - Use Default Weight Scheme in Ranked Retrieval");
-    println!(":scheme t || :scheme tdidf - Use 'td-idf' Weight Scheme in Ranked Retrieval");
+    println!(":scheme t || :scheme tfidf - Use 'tf-idf' Weight Scheme in Ranked Retrieval");
     println!(":scheme o || :scheme okapi - Use Okapi BM25 Weight Scheme in Ranked Retrieval");
     println!(":scheme w || :scheme wacky - Use Wacky Weight Scheme in Ranked Retrieval");
     println!();
